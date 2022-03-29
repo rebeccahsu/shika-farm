c1082ce5a43439b622ffe0da01b3e58bf0d38fdb
tsaooneone <tcw1126one@gmail.com>
2022-03-29 20:02:37 +0800

    feat: 新增結帳流程step1，修改checkout.js，新增商品圖 style: 修改aside登出icon

---
+++ src/js/checkout.js
@@ -0,0 +1,22 @@
+new Vue({
+    el:'#step1',
+    data:{
+        images:{
+            strawberry: '../img/porducts/strawberry.png',
+            coffee: '../img/porducts/coffee.png',
+            papaya: '../img/porducts/papaya.png',
+            apple: '../img/porducts/apple.png',
+            pannacotta: './img/porducts/panna_cotta.png',
+            icecream: '../img/porducts/ice_cream.png',
+            creamroll: '../img/porducts/cream_roll.jpg',
+            cheesecake: '../img/porducts/cheese_cake.jpg',
+            blanketg: '../img/porducts/blanket_g.jpg',
+            blanketr: '../img/porducts/blanket_r.jpg',
+            blankety: '../img/porducts/blanket_y.jpg',
+            animalcookies: './img/porducts/animal_cookies.jpg'
+        },
+        products:[
+            {info:'羊毛毯(綠)', price:'NT$ 499', toatl:'NT$ 499'},
+        ],
+    },
+});
