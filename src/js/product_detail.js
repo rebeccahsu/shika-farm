2cf2fd76c36ffdffe366b72f663c116442469528
neko0104 <kunpao0104@gmail.com>
2022-04-03 01:19:34 +0800

    style:商品內頁 調整頁面、RWD

--- src/js/product_detail.js
+++ src/js/product_detail.js
@@ -4,31 +4,42 @@ new Splide('.splide01').mount();
 
 document.addEventListener('DOMContentLoaded', function () {
     var main_1 = new Splide('.splide01', {
-        type: 'fade',
+        type: 'splide',
         rewind: true,
         pagination: false,
         // arrows: false,
         // fixedHeight: 500,
         padding: { bottom: 10 },
+        // マウスホイールによるスライダーの移動を有効
+        wheel       : true,
+        releaseWheel: true,
 
     });
 
 
     var thumbnails_1 = new Splide('#thumbnail-slider1', {
-        fixedWidth: 100,
-        fixedHeight: 100,
-        gap: 3,
+        fixedWidth:183,
+        fixedHeight:183,
+        gap:10,
         rewind: true,
         pagination: false,
         cover: true,
-        isNavigation: true,
+        isNavigation: false,
         arrows: false,
-        padding: { left: 0, right: 5 },
+        padding: { top: 10, left: 0, right: 0 },
         breakpoints: {
+            1200:{
+                fixedWidth:163,
+                fixedHeight:163,
+            },
+            992:{
+                fixedWidth:130,
+                fixedHeight:130,
+                gap:10,
+            },
             768: {
                 // destroy:true,  //廢棄會將此段套件失效
-                fixedWidth: 100,
-                fixedHeight: 100,
+                // autoWidth: false
             },
         },
     });
@@ -42,13 +53,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
 var main_2 = new Splide('#splide02', {
     fixedWidth: '80',
-    // fixedHeight: 100,
     type: 'fade',
     rewind: true,
     pagination: true,
     arrows: true,
-    // fixedHeight: 500,
-    padding: { right:10,left:10, bottom: 10 },
+    padding: { right: 10, left: 10, bottom: 10 },
     // mediaQuery: 'min',  //mediaQuery:'min'or'max'配合breakpoints: 尺寸
     // destroy: false,  //廢棄會將此段套件區塊會display:none
     // breakpoints: {
@@ -57,36 +66,36 @@ var main_2 = new Splide('#splide02', {
     //     }}
 }).mount();
 
-if (window.innerWidth > 767.98) {  removeSplide(); }
+if (window.innerWidth > 991.98) { removeSplide(); }
 
 
-        // for resize
-        function addSplide() {
-            $('.pd_recommend').attr("id", "splide02")
-            $('.pd_recommend_list').parent("div").addClass('splide__track');
-            $('.pd_recommend_list').addClass('splide__list');
-            $('.pd_recommend_list').find("li").addClass('splide__slide');
+// for resize
+function addSplide() {
+    $('.pd_recommend').attr("id", "splide02")
+    $('.pd_recommend_list').parent("div").addClass('splide__track');
+    $('.pd_recommend_list').addClass('splide__list');
+    $('.pd_recommend_list').find("li").addClass('splide__slide');
+}
+
+function removeSplide() {
+    $('.pd_recommend').removeAttr("id", "splide02")
+    $('.pd_recommend_list').parent("div").removeClass('splide__track');
+    $('.pd_recommend_list').removeClass('splide__list');
+    $('.pd_recommend_list').find("li").removeClass('splide__slide');
+}
+
+$(window).on('resize', function () {
+    console.log(innerWidth);
+    if (window.innerWidth > 991.98) {
+        if ($('.pd_recommend').has('#splide02')) {
+            removeSplide();
+
+            console.log('object');
         }
-
-        function removeSplide() {
-            $('.pd_recommend').removeAttr("id", "splide02")
-            $('.pd_recommend_list').parent("div").removeClass('splide__track');
-            $('.pd_recommend_list').removeClass('splide__list');
-            $('.pd_recommend_list').find("li").removeClass('splide__slide');
-        }
-
-        $(window).on('resize', function () {
-            console.log(innerWidth);
-            if (window.innerWidth > 767.98) {
-                if($('.pd_recommend').has('#splide02')){
-                    removeSplide();
-
-                    console.log('object');
-                }
-            }else{
-                addSplide();
-            }
-        })
+    } else {
+        addSplide();
+    }
+})
 
 // ===========================
 
