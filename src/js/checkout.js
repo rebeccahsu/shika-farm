fd63fa0a017f1604022fe491a614e6955a25a953
tsaooneone <tcw1126one@gmail.com>
2022-04-02 22:32:49 +0800

    feat: 修改checkout頁面,修改member_mg.html & checkout.html的js檔案路徑, 修改checkout.js

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,46 +1,50 @@
-// ========== 結帳步驟一 ==========
 new Vue({
-    el:'.stepA',
-    data:{
-        images:{
-            strawberry: '../img/porducts/strawberry.png',
-            coffee: '../img/porducts/coffee.png',
-            papaya: '../img/porducts/papaya.png',
-            apple: '../img/porducts/apple.png',
-            pannacotta: './img/porducts/panna_cotta.png',
-            icecream: '../img/porducts/ice_cream.png',
-            creamroll: '../img/porducts/cream_roll.jpg',
-            cheesecake: '../img/porducts/cheese_cake.jpg',
-            blanketg: '../img/porducts/blanket_g.jpg',
-            blanketr: '../img/porducts/blanket_r.jpg',
-            blankety: '../img/porducts/blanket_y.jpg',
-            animalcookies: './img/porducts/animal_cookies.jpg'
+	el: ".stepA",
+	data: {
+		products: [
+			{
+				id: 0001,
+				img: "../img/porducts/blanket_g.jpg",
+				info: "羊毛毯(綠)",
+				price: 499,
+				quantity: 1,
+			},
+			{
+                id: 0002,
+				img: "./img/porducts/animal_cookies.jpg",
+				info: "動物餅乾",
+				price: 200,
+				quantity: 1,
+			},
+		],
+	},
+	methods: {
+		// 執行add這個function, index->陣列裡的第幾個物件 n->加1減1的值
+		add(index, n) {
+			// 如果按下加減後等於0
+			if (this.products[index].quantity + n == 0) {
+				return; // 就結束顯示預設數字 1
+			}
+			this.products[index].quantity += n; // 不然就可以做加減
+		},
+        //點擊垃圾桶 刪除
+        del(index){
+            this.products.splice(index,1);
         },
-        products:[
-            {info:'羊毛毯(綠)', price:'NT$ 499',quantity: 1, total:'NT$ 499', freight:'NT$ 0'},
-        ],
-    },
+	},
+    computed:{
+        // 總計 執行function
+        sumTotal:function(){
+            //預設 total=0
+            let total = 0;
+            // vue的products陣列執行foreach迴圈
+            this.products.forEach(el => {
+                //陣列裡的物件執行這個涵式
+                // 總計 = 物件的價格 * 物件的數量
+                total += el.price * el.quantity;
+            });
+            //把值傳回sumTotal這個變數
+            return total;
+        }
+    }
 });
-// ========== 結帳步驟二 ==========
-// new Vue({
-//    el:'.stepB',
-//    data:{
-//         images:{
-//             strawberry: '../img/porducts/strawberry.png',
-//             coffee: '../img/porducts/coffee.png',
-//             papaya: '../img/porducts/papaya.png',
-//             apple: '../img/porducts/apple.png',
-//             pannacotta: './img/porducts/panna_cotta.png',
-//             icecream: '../img/porducts/ice_cream.png',
-//             creamroll: '../img/porducts/cream_roll.jpg',
-//             cheesecake: '../img/porducts/cheese_cake.jpg',
-//             blanketg: '../img/porducts/blanket_g.jpg',
-//             blanketr: '../img/porducts/blanket_r.jpg',
-//             blankety: '../img/porducts/blanket_y.jpg',
-//             animalcookies: './img/porducts/animal_cookies.jpg'
-//         },
-//         products:[
-//             {info:'羊毛毯(綠)', price:'NT$ 499', quantity: 1,total:'NT$ 499', freight:'NT$ 0'},
-//         ],
-//     },
-// });
