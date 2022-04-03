02cbbd33598025d26dafcbde3ce62199bbd31a26
rebeccahsu <rebeccahsuhth@gmail.com>
2022-04-03 16:05:18 +0800

    chore: 安裝 gsap, feat: 首頁動畫

--- src/js/index.js
+++ src/js/index.js
@@ -19,3 +19,43 @@ new Vue ({
         ],
     }  
 })
+
+gsap.to('.sun', {
+    duration: 15,
+    rotation: 360,
+    repeat: -1,
+    // repeatRefresh: true,
+});
+
+gsap.to( ['.top-cloud1', '.top-cloud3'], {
+    repeat: -1,
+    repeatRefresh: true,
+    keyframes: [
+        { duration: 2, y: 10},
+        { duration: 2, y: 0},
+    ]
+});
+
+gsap.to( ['.top-cloud2', '.top-cloud4'], {
+    repeat: -1,
+    repeatRefresh: true,
+    keyframes: [
+        { duration: 3, y: 15},
+        { duration: 2, y: 0},
+    ]
+});
+
+const t6 = gsap.timeline({
+    defaults: {
+      duration: .6,
+    },
+  });
+  
+t6.to('.top-horse', { x: '5%', })
+    .to('.top-sheep', { x: '5%', })
+    .to('.top-cow', { x: '-5%' });
+
+t6.to('.top-deer', { x: '5%' })
+    .to('.top-alpaca', { x: '-5%' });
+
+gsap.to('.top-pig', { x: '-5%', });
