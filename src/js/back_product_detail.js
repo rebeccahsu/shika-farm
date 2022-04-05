14478ba7298f487de6a244b215d1f0938c889dfc
neko0104 <kunpao0104@gmail.com>
2022-04-05 23:42:01 +0800

    feat:後台商品頁 主圖選擇、提醒售價fixbug

--- src/js/back_product_detail.js
+++ src/js/back_product_detail.js
@@ -8,16 +8,17 @@
 // ========================================
 // 提示價錢未寫、成本比售價高
 
-$('#prd_price').on('blur',check_price);
-$('#prd_cost').on('blur',check_cost);
+$('#prd_price').on('blur', check_price);
+$('#prd_cost').on('blur', check_cost);
 
 
 function check_price() {
     if ($('#prd_price').val() == "") {
         $('#prd_price').addClass('warn');
         $('p[for="prd_price"]').html('售價<span>*請輸入售價</span>');
-    } else if ($('#prd_price').val() < $('#prd_cost').val()) {
+    } else if (parseInt($('#prd_price').val()) < parseInt($('#prd_cost').val())) {
         $('#prd_price').addClass('warn');
+        $('p[for="prd_price"]').html('售價<span>*售價比成本低</span>');
     } else {
         $('#prd_price').removeClass('warn');
         $('p[for="prd_price"]').html('售價');
@@ -28,8 +29,9 @@ function check_cost() {
     if ($('#prd_cost').val() == "") {
         $('#prd_cost').addClass('warn');
         $('p[for="prd_cost"]').html('成本<span>*請輸入成本</span>');
-    } else if ($('#prd_price').val() < $('#prd_cost').val()) {
+    } else if (parseInt($('#prd_price').val()) < parseInt($('#prd_cost').val())) {
         $('#prd_price').addClass('warn');
+        $('p[for="prd_price"]').html('售價<span>*售價比成本低</span>');
     } else {
         $('#prd_cost').removeClass('warn');
         $('p[for="prd_cost"]').html('成本');
@@ -38,9 +40,9 @@ function check_cost() {
 
 // ========================================
 // 庫存、商品名稱不可空白，種類檢查
-$('#prd_name').on('blur',check_name);
-$('#prd_inStock').on('blur',check_inStock);
-$('#prd_kind').on('change',check_kind);
+$('#prd_name').on('blur', check_name);
+$('#prd_inStock').on('blur', check_inStock);
+$('#prd_kind').on('change', check_kind);
 
 
 function check_inStock() {
@@ -63,11 +65,11 @@ function check_name() {
     }
 }
 
-function check_kind(){
+function check_kind() {
     if ($('#prd_kind')[0].options.selectedIndex == 0) {
         $('#prd_kind').addClass('warn');
         $('p[for="prd_kind"]').html('商品類別<span>*請選擇商品類別</span>');
-    }else{
+    } else {
         // console.log('nn');
         $('#prd_kind').removeClass('warn');
         $('p[for="prd_kind"]').html('商品類別');
@@ -78,27 +80,162 @@ function check_kind(){
 // ================================================
 // 商品介紹intro區塊
 
-// 刪除區塊
-$(document).on('click',(e)=>{
-    if($(e.target).hasClass('bi-x')){
-        console.log($(e.target).closest('li'));
+//刪除區塊
+$(document).on('click', (e) => {
+    if ($(e.target).hasClass('bi-x')) {
+        // console.log($(e.target).closest('li'));
         $(e.target).closest('li').remove();
     }
 })
 
 // 新增區塊
-$('#add_prdIntroduce').on('click',(e)=>{
+$('#add_prdIntroduce').on('click', (e) => {
     e.preventDefault;
-    if($('.prd_introduce').length == 2 || $('.prd_introduce').length < 5){
+    if ($('.prd_introduce').length == 2 || $('.prd_introduce').length < 5) {
         $('#introduce_area').append(`<li class="prd_introduce">
         <div class="select_image"><span>上傳圖片</span></div>
         <textarea type="text" class="prd_introduce_input" row="1" cols="2" maxlength="30"></textarea>
         <i class="bi bi-x"></i>
     </li>`);
-    }else{
+    } else {
         alert("商品介紹區塊最多只能5個");
     }
 })
+
+// 商品介紹intro區塊end
+// =================================================
+
+// 關閉按鈕和範圍==============
+
+$('#back_bg').on('click', closebg)
+$('.bi-x').on('click', closebg);
+$('#pop_box').on('click', (e) => { e.stopPropagation(); })
+$('.cancel').on('click', closebg)
+
+function closebg(t) {
+    // console.log(t);
+    $('#back_bg').removeClass("-on");
+}
+// 關閉按鈕和範圍==============
+// 圖片上傳
+$('#topImages').on('click', (e) => {
+    e.preventDefault;
+    $('#back_bg').addClass("-on");
+})
+
+// 樣式變更
+function add_shadow(el) {
+    el.classList.add("y_shadow");
+}
+function remove_shadow(el) {
+    el.classList.remove("y_shadow");
+}
+
+$('.select_topImage').on("dragover", (e) => {
+    e.preventDefault;
+    add_shadow(e.target);
+})
+
+$('.select_topImage').on("dragleave", (e) => {
+    e.preventDefault;
+    remove_shadow(e.target);
+})
+
+
+$('.select_topImage').on("drop", (e) => {
+    e.preventDefault;
+    add_shadow(e.target);
+})
+
+
+
+// 抓檔案
+var preview_img = function(file) {
+    var reader = new FileReader(); // 用來讀取檔案
+    reader.readAsDataURL(file); // 讀取檔案
+    reader.addEventListener("load", function () {
+        console.log(reader.result);
+        // console.log(preview_area);
+        // let img_str = '<img src="' + reader.result + '>';
+        // preview_area.innerHTML = img_str;
+        // console.log(img_str);
+        return reader.result;
+    });
+};
+
+// 呼叫FileReader轉base64
+var preview_el;
+var preview_img = function(file){
+
+    var reader = new FileReader(); // 用來讀取檔案
+    reader.readAsDataURL(file); // 讀取檔案
+    reader.addEventListener("load", function () {
+      //console.log(reader.result);
+      let img_str = '<img src="' + reader.result + '" class="preview_img">';
+      preview_el.innerHTML = img_str;
+    //   console.log(img_str);
+    });
+  };
+
+
+$("#file1").on("change", (e)=>{
+    // console.log($("#file1")[0]);
+    if($("#file1")[0].files.length > 0){
+        preview_el = $(".select_topImage1")[0];
+        preview_img($("#file1")[0].files[0]);
+      }else{
+        preview_el = $(".select_topImage1")[0];
+        preview_el.innerHTML = '<span>請上傳圖片</span>';
+      }
+})
+
+$("#file2").on("change", (e)=>{
+    // console.log($("#file2")[0]);
+    if($("#file2")[0].files.length > 0){
+        preview_el = $(".select_topImage2")[0];
+        preview_img($("#file2")[0].files[0]);
+      }else{
+        preview_el = $(".select_topImage2")[0];
+        preview_el.innerHTML = '<img src="./img/logo_16B1AA.svg" alt="">';
+      }
+})
+
+$("#file3").on("change", (e)=>{
+    // console.log($("#file3")[0]);
+    if($("#file3")[0].files.length > 0){
+        preview_el = $(".select_topImage3")[0];
+        preview_img($("#file3")[0].files[0]);
+      }else{
+        preview_el = $(".select_topImage3")[0];
+        preview_el.innerHTML = '<img src="./img/logo_16B1AA.svg" alt="">';
+      }
+})
+
+$("#file4").on("change", (e)=>{
+    // console.log($("#file4")[0]);
+    if($("#file4")[0].files.length > 0){
+        preview_el = $(".select_topImage4")[0];
+        preview_img($("#file4")[0].files[0]);
+      }else{
+        preview_el = $(".select_topImage4")[0];
+        preview_el.innerHTML = '<img src="./img/logo_16B1AA.svg" alt="">';
+      }
+})
+
+$(".upload").on('click',(e)=>{
+    e.preventDefault();
+    let img_base64 = [];
+    for(let i=0; i<$('.preview_img').length; i++){
+        let img = $('.preview_img')[i].getAttribute('src');
+       if(img !== "./img/logo_16B1AA.svg" || img == null){
+           img_base64.push(img);
+        //    console.log(img_base64);
+           sessionStorage.setItem("top_img", JSON.stringify(img_base64));
+       }
+    }
+    closebg();
+})
+
 
 // =================================================
 /*
@@ -129,34 +266,37 @@ var product = {
 */
 
 // 檢查必填欄位、取消按鍵
-$('#submit').on('click',(e)=>{
+$('#submit').on('click', (e) => {
     e.preventDefault;
-    check_price();
     check_cost();
+    check_price();
     check_inStock();
     check_name();
     check_kind()
-    if($('#prd_name').val() == "" || $('#prd_cost').val() == "" || $('#prd_price').val() == "" || $('#prd_inStock').val() == "" || $('#prd_kind').val() == "n"){
-        // window.location.hash = "#prd_name";
+    if ($('#prd_name').val() == "" || $('#prd_cost').val() == "" || $('#prd_price').val() == "" || $('#prd_inStock').val() == "" || $('#prd_kind').val() == "n") {
+
         // console.log($("#prd_name")[0].offsetTop);
-        window.scrollTo(0,$("#prd_name")[0].offsetTop-100);
+
+        window.scrollTo(0, $("#prd_name")[0].offsetTop - 100);
         alert('有欄位沒寫');
-        }else{ //有資料庫時要改寫存入地點
+    } else { //有資料庫時要改寫存入地點
+        console.log('還沒寫好');
+        /*
+        let intro = $('.prd_introduce');
+        $('.prd_introduce').each((i, v) => {
+            console.log(i, v);
 
-
-            $('.prd_introduce').forEach(i => {
-                console.log(i);
-                
-                var product = {
+            var product = {
                 //prd_number: `${$('.prd_number')[i].innerText}`,
                 prd_name: $('#prd_name').val(), // <-> "varchar"
                 prd_cost: $('#prd_cost').val(),
                 prd_price: $('#prd_price').val(),
                 prd_inStock: $('#prd_inStock').val(),
                 prd_pic: `${$('.select_topImage1')[0].getAttribute("src")}`, //(主圖網址) "./img/producds/${品號}_top01"  <-> "varchar"
-                prd_condition: `${$('#prd_condition').val()}`,
+                prd_condition: `未上架`, //新增=未上架,修改=抓值${$('#prd_condition').val()}
                 prd_topImage: [`${$('.select_topImage1')[0].getAttribute("src")}`], // (網址) ["./img/producds/${品號}_top01"] max=4
-                prd_intro: [{scr:`${$('.pd_intro_pic')[i].closest('img').getAttribute("src")}`,text:`${$('.prd_introduce_input')[i].innerText}`}], //商品介紹,"varchar" [{"src","text"}]  (網址)[{"./img/producds/${品號}_01"},{}] max=5
+                prd_intro: [{ scr: `${$('.prd_introduce')[i].querySelectorAll('img')[0].getAttribute("src")}`, text: `${$('.prd_introduce_input')[i].innerText}` }], //商品介紹,"varchar" [{"src","text"}]  (網址)[{"./img/producds/${品號}_01"},{}] max=5
+                // 前台是pd_intro 後台是prd_introduce
                 prd_kind: `${$('#prd_kind').val()}`, //和資料庫關聯
                 prd_slog: [`${$('#prd_slog1').val()}`, `${$('#prd_slog2').val()}`], //促銷標語 max=2
                 prd_ingredient: `${$('#prd_ingredient').val()}`, //innerHtml 商品規格
@@ -164,17 +304,16 @@ $('#submit').on('click',(e)=>{
 
             console.log(product);
 
-            });
-
-
-        }
-
+            // location.href = "./back_products.html";
+        });
+        */
+    }
 })
 
 // 取消
-$('#cancel').on('click',(e)=>{
+$('#cancel').on('click', (e) => {
     e.preventDefault;
-    if (confirm("確定要取消嗎？資料還沒存檔唷！")){
+    if (confirm("確定要取消嗎？資料還沒存檔唷！")) {
         location.href = "./back_products.html";
     }
 })
