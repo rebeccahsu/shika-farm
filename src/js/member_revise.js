fb1b2902732e9150905b7ee82d5217f230195e33
tsaooneone <tcw1126one@gmail.com>
2022-04-06 09:32:15 +0800

    feat: 新增購物車彈窗, 新增會員地址欄位 style:修改彈窗內容樣式

--- src/js/member_revise.js
+++ src/js/member_revise.js
@@ -3,30 +3,59 @@
 $.ajax({
 	url: "../JSON/city.json",
 	method: "GET",
-	success: function (res) {
-		// console.log(res);
-		for (let i = 0; i < res.length; i++) {
-			$("#city").append(`
-                <option value="${res[i].name}">${res[i].name}</option>
-            `);
+    success: function (res) {
+      function state(a) {
+        for (let i = 0; i < res[a].districts.length; i++) {
+          //console.log(res[a].districts[i].name)
+          $("#area").append(
+            "<option value=" +
+              res[a].districts[i].name +
+              ">" +
+              res[a].districts[i].name +
+              "</option>"
+          );
+        }
+      }
+      // 初始畫面的縣市呈現
+      $.each(res, function (index, ele) {
+        // console.log(index);
+        $("#city").append(
+          "<option value=" + index + ">" + res[index].name + "</option>"
+        );
+      });
+      // 改變縣市
+      $("#city").on("change", function () {
+        //console.log($(this).children("option:selected").val());
+        var num = $(this).children("option:selected").val();
+        $("#area").find("option").remove();
+        state(num); //num 是縣市的索引值  state是縣市區域的函式
+      });
+    }
+});
+	// success: function (res) {
+	// 	// console.log(res);
+	// 	for (let i = 0; i < res.length; i++) {
+	// 		$("#city").append(`
+    //             <option value="${res[i].name}">${res[i].name}</option>
+    //         `);
 
-			$("select#city").on("change", function () {
-				for (let j = 0; j < res[i].districts.length; j++) {
+	// 		$("select#city").on("change", function () {
+	// 			for (let j = 0; j < res[i].districts.length; j++) {
                     
-					let city = $('#city :selected').val();
+	// 				let city = $('#city :selected').val();
     
                    
                    
-					if(city == res[i].name) {
-                        console.log(res[j].districts)
+	// 				if(city == res[i].name) {
+    //                     console.log(res[j].districts)
 
-					    $('#area').append(`
-					    <option>${res[j].districts.name}</option>
-					    `)
-					}
-				}
-			});
-		}
+	// 				    $('#area').append(`
+	// 				    <option>${res[j].districts.name}</option>
+	// 				    `)
+	// 				}
+	// 			}
+	// 		});
+	// 	}
 		// $("select#city").on("change", function () {
 		// 	console.log(res);
 		//     for (let i = 0; i < res.length; i++) {
@@ -39,14 +68,14 @@ $.ajax({
 		//     //             <option>${res[i][j].name}</option>
 		//     //         `);
 		// 	// 	}
-		// 	// }
-		// });
-	},
-});
+// 		// 	// }
+// 		// });
+// 	},
+// });
 
 
 var el =document.querySelector('.ch_pw');
 el.addEventListener('click' ,function(e){
 e.preventDefault();
 console.log('test');
-},false);
+});
