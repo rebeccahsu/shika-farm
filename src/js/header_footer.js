41e81b636a83936c04d1d0b13180c5c5f2743f5c
rebeccahsu <rebeccahsuhth@gmail.com>
2022-04-07 22:47:44 +0800

    fix: 圖片路徑修正

--- src/js/header_footer.js
+++ src/js/header_footer.js
@@ -99,14 +99,14 @@ new Vue({
 		let products = [
 			{
 				id: "0001",
-				img: "../img/products/blanket_g.jpg",
+				img: "./img/products/blanket_g.jpg",
 				info: "羊毛毯(綠)",
 				price: 499,
 				quantity: 1,
 			},
 			{
 				id: "0002",
-				img: "../img/products/animal_cookies.jpg",
+				img: "./img/products/animal_cookies.jpg",
 				info: "動物餅乾",
 				price: 200,
 				quantity: 1,
