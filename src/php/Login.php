7c85003fdb4aa3bbd5d4292908e10e38d6f5e6b5
neko0104 <kunpao0104@gmail.com>
2022-04-14 11:47:03 +0800

    feat: login功能雛形

---
+++ src/php/Login.php
@@ -0,0 +1,73 @@
+<?php
+
+// include("Connection.php");
+
+//取得資料
+//透過php取得前端資料並解譯
+$_POST = json_decode(file_get_contents("php://input"), true);
+// 另新變數=資料內文
+$Mail = $_POST["Mail"];
+$PWD = $_POST["Password"];
+
+$db_host = "127.0.0.1";
+$db_user = "root";
+$db_pass = "password";
+$db_select = "TFD105_G6";
+
+$dsn = "mysql:host=".$db_host.";dbname=".$db_select;
+
+$pdo = new PDO($dsn, $db_user, $db_pass);
+
+//---------------------------------------------------
+
+
+
+
+       //建立SQL
+       // PDO執行的事情
+    //    將原本的王曉明和密碼改為變數即可
+      //  $sql = "INSERT INTO member(Name, PWD, CreateDate) VALUES ('$Name', '$PWD', NOW())";
+// select 資料 where name=輸入的帳號  and 串接條件  PWD=輸入的密碼
+$sql = "SELECT * FROM member where EMAIL= ? and PASSWORD = ? " ;
+       //執行 -> 就是PHP 的.       exec執行鍵入$sql內的文字
+    
+               // 準備  封裝好先不執行
+$statement = $pdo->prepare($sql);
+// 過濾輸入的值   有幾的欄位就會濾幾次
+$statement->bindValue( 1 ,$Mail);
+$statement->bindValue( 2 ,$PWD);
+
+// 執行
+$statement->execute();
+
+//抓出全部且依照順序封裝成一個二維陣列
+$data = $statement->fetchAll();
+
+
+// 實作中 不會讓使用者看到所有資料，而資料庫有PK，不會有重複，可以找資料>0筆
+if(count($data) > 0){
+   // 啟用Session  Session是在伺服器端建立，預設存在時間是30分鐘
+   session_start();
+   // 當同個瀏覽器有舊的PHPSESSID，就更新PHPSESSID等資料
+   if ($_SESSION != null) {
+      session_regenerate_id();
+  }
+   $_SESSION['ID'] = $data[0]["ID"];
+   $_SESSION['NAME'] = $data[0]["NAME"];
+   $_SESSION['member'] = (array) $data;
+   $_SESSION['successful'] = true;
+   $_SESSION['message'] = '登入成功，歡迎回來';
+   echo json_encode($_SESSION);  
+   
+}else{
+   $_SESSION['ID'] = "";
+   $_SESSION['NAME'] = "";
+   $_SESSION['member'] = "";
+   $_SESSION['successful'] = false;
+   $_SESSION['message'] = "帳號或密碼錯誤！";
+   echo json_encode($_SESSION);
+
+}
+
+
+?>
