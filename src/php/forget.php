05ac359926a26f081826f7462c65304c28dcb0f7
neko0104 <kunpao0104@gmail.com>
2022-04-15 20:50:33 +0800

    feat: 忘記密碼開發中

--- src/php/forget.php
+++ src/php/forget.php
@@ -12,29 +12,31 @@ $statement = $pdo->prepare($sql);
 $statement->bindValue( 1 ,$mail);
 $statement->execute();
 
+$resultCount = $statement -> rowCount();
 $data = $statement->fetchAll();
 
-if(count($data) > 0){
-    $resp["TOKEN"] = creatTOKEN();
+
+if(($resultCount) > 0){
+    $resp["TOKEN"] = creatTOKEN($pdo,$mail);
     $resp["successful"] = true;
     $resp["NAME"] = $data[0]["NAME"];
 
 }else{
-    $resp["successful"] = true;
+    $resp["successful"] = false;
 }
 
 echo json_encode($resp);
 
 
 // 產生token
-function creatTOKEN(){
+function creatTOKEN($pdo,$mail){
     $Strings = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $TOKEN = substr(str_shuffle($Strings), 0, 45);
-    $sql2 = "UPDATE set MEMBER TOKEN='?' where EMAIL='?'";
+    $sql2 = "UPDATE set MEMBER TOKEN=':TOKEN' where EMAIL=':EMAIL'";
 
     $statement2 = $pdo->prepare($sql2);
-    $statement2->bindValue( 1 ,$TOKEN);
-    $statement2->bindValue( 2 ,$mail);
+    $statement2->bindValue(":TOKEN",$TOKEN);
+    $statement2->bindValue(":EMAIL",$mail);
     $statement2->execute();
 
     return $TOKEN;
