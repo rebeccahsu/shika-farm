38fb9e1e9f98f29a7e6b1944e8d3551bccc51b2c
tsaooneone <tcw1126one@gmail.com>
2022-04-18 20:43:11 +0800

    feat: 抓到資料庫會員資料、 新增memeber.php、更改checkout下拉選單箭頭圖片路徑

--- src/php/member.php
+++ src/php/member.php
@@ -1,3 +1,18 @@
 <?php
-    
+     include ("./connection.php");
+
+     //取得資料
+     //透過php取得前端資料並解譯
+     $body = json_decode(file_get_contents("php://input"), true);
+     // 另新變數=資料內文
+    //  $serch_value = $input_value['input_value'];
+ 
+     $sql = "select * from MEMBER where ID=:id";
+     $stmt = $pdo->prepare($sql);
+     $stmt->bindValue(":id", $body["id"]);
+     $stmt->execute();
+     $result = $stmt->fetchAll(); //把結果放在二為陣列裡
+     
+     echo json_encode($result);
+    //  $resultCount = $stmt->rowCount();
 ?>
