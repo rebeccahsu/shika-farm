448baf1f5f48373408858d8f57557ff81dc835d3
neko0104 <kunpao0104@gmail.com>
2022-04-18 18:52:19 +0800

    feat: 列表刪除商品

---
+++ src/php/back_products_delete.php
@@ -0,0 +1,38 @@
+<?php
+
+include("connection.php");
+
+//取得資料
+//透過php取得前端資料並解譯
+$input_value = json_decode(file_get_contents("php://input"), true);
+// 另新變數=資料內文
+$id = $input_value['ID'];
+
+       //---------------------------------------------------
+
+       //建立SQL語法                             
+       $sql = "delete from PRODUCT where ID =:ID ;" ;
+
+       //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
+       $statement = $pdo->prepare($sql);
+
+        //查詢執行結果筆數
+       $resultCount = $statement -> rowCount();
+       $statement->bindValue(":ID", $id);
+       // 執行上面的設定
+       $statement->execute();
+
+           //根據執行結果筆數判斷是否執行成功並設定回應內容   || $resultCount2 > 0
+    //    if ($resultCount > 0){
+              $respBody['successful'] = true;
+              $respBody['message'] = "刪除成功";
+              $respBody['end'] = $statement -> rowCount();
+              echo json_encode($respBody);
+
+    //    }else{
+    //           $respBody['successful'] = false;
+    //           $respBody['message'] = "unfinded data";
+    //    }
+
+
+?>
