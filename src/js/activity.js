fa39073bec6925486e43aee480084aebf4587543
charmeee13 <yuin1995@gmail.com>
2022-04-21 19:03:08 +0800

    feat:ajax

--- src/js/activity.js
+++ src/js/activity.js
@@ -1,11 +1,54 @@
 
-new Vue({
+const vm = new Vue({
     el: '#app',
     data() {
         return {
+            xxx: 'yyy',
+            showList: [],
+            s: 's',
+            slide: 'slide',
             modal_open: null,
             activeIndex: 0,
             period: null,
+            lable_icons:
+                [
+                    {
+                        key: 0,
+                        img: './img/activity/sheep 2.png',
+                        checked: true,
+                        type: 'sheep'
+                    },
+                    {
+                        key: 1,
+                        img: './img/activity/deer 2.png',
+                        checked: false,
+                        type: 'deer'
+                    },
+                    {
+                        key: 2,
+                        img: './img/activity/alpaca 2.png',
+                        checked: false,
+                        type: 'alpaca'
+                    },
+                    {
+                        key: 3,
+                        img: './img/activity/cow 2.png',
+                        checked: false,
+                        type: 'cow'
+                    },
+                    {
+                        key: 4,
+                        img: './img/activity/pig 2.png',
+                        checked: false,
+                        type: 'pig'
+                    },
+                    {
+                        key: 5,
+                        img: './img/activity/horse 2.png',
+                        checked: false,
+                        type: 'horse'
+                    }
+                ],
             all_data:
 
                 [
@@ -96,36 +139,36 @@ new Vue({
                         button_bg: '#FF8671',
                         icon_image: '',
                         list: [
-                            {
-                                name: '3',
-                                title: '乳牛吃飽飽',
-                                image: './img/activity/eatfresh.jpg',
-                                icon_img: '',
-                                desc: '來到Sìkha牧場，就可以憑門票兌換一把草，可以更近一步的跟可愛的乳牛互動餵牠吃草吃到飽！！',
-                                data: ['3/11', '3/13']
-                            },
-                            {
-                                name: '3',
-                                title: '擠ㄋㄟㄋㄟ',
-                                image: './img/activity/milkmaker.jpg',
-                                icon_img: './img/activity/cow1.png',
-                                desc: '幫乳牛媽媽擠ㄋㄟㄋㄟ，讓小朋友們知道，原來牛奶是從這裡來的呢！。透過工作人員教學說明後，我們可以親手擠出白色的牛奶，配合乳牛媽媽的生理時間，提供每日３個場次可以體驗喔！',
-                                data: ['3/11', '3/13'],
-                                time_period: ["9:00-9:30", "10:00-10:30", "14:00-14:30"],
-                                remain: '3',
-                                reserve: '點我預約'
-                            },
-                            {
-                                name: '3',
-                                title: 'DIY ㄋㄟㄋㄟ 餅乾',
-                                image: './img/activity/makecookie.jpg',
-                                icon_img: './img/activity/cookie_icon.png',
-                                desc: '奶味香濃的餅乾大人小孩都喜歡！但有親手做過餅乾嗎？Sìkha牧場提供新鮮牛奶讓各位大朋友小朋友來這親手製作出香濃可口的ㄋㄟㄋㄟ餅乾 ! ? 簡單三步驟就能讓你完成屬於自己的肥皂 ，讓Sìkha肥皂滲透你的肌膚，享受肌膚絲緞般的觸感，自用送禮兩相宜，還不快來體驗！ ',
-                                data: ['3/11', '3/13'],
-                                time_period: ["9:00-10:30", "13:00-14:30", "15:00-16:30"],
-                                remain: '9',
-                                reserve: '點我預約'
-                            }
+                            // {
+                            //     name: '3',
+                            //     title: '乳牛吃飽飽',
+                            //     image: './img/activity/eatfresh.jpg',
+                            //     icon_img: '',
+                            //     desc: '來到Sìkha牧場，就可以憑門票兌換一把草，可以更近一步的跟可愛的乳牛互動餵牠吃草吃到飽！！',
+                            //     data: ['3/11', '3/13']
+                            // },
+                            // {
+                            //     name: '3',
+                            //     title: '擠ㄋㄟㄋㄟ',
+                            //     image: './img/activity/milkmaker.jpg',
+                            //     icon_img: './img/activity/cow1.png',
+                            //     desc: '幫乳牛媽媽擠ㄋㄟㄋㄟ，讓小朋友們知道，原來牛奶是從這裡來的呢！。透過工作人員教學說明後，我們可以親手擠出白色的牛奶，配合乳牛媽媽的生理時間，提供每日３個場次可以體驗喔！',
+                            //     data: ['3/11', '3/13'],
+                            //     time_period: ["9:00-9:30", "10:00-10:30", "14:00-14:30"],
+                            //     remain: '3',
+                            //     reserve: '點我預約'
+                            // },
+                            // {
+                            //     name: '3',
+                            //     title: 'DIY ㄋㄟㄋㄟ 餅乾',
+                            //     image: './img/activity/makecookie.jpg',
+                            //     icon_img: './img/activity/cookie_icon.png',
+                            //     desc: '奶味香濃的餅乾大人小孩都喜歡！但有親手做過餅乾嗎？Sìkha牧場提供新鮮牛奶讓各位大朋友小朋友來這親手製作出香濃可口的ㄋㄟㄋㄟ餅乾 ! ? 簡單三步驟就能讓你完成屬於自己的肥皂 ，讓Sìkha肥皂滲透你的肌膚，享受肌膚絲緞般的觸感，自用送禮兩相宜，還不快來體驗！ ',
+                            //     data: ['3/11', '3/13'],
+                            //     time_period: ["9:00-10:30", "13:00-14:30", "15:00-16:30"],
+                            //     remain: '9',
+                            //     reserve: '點我預約'
+                            // }
                         ]
                     },
                     {
@@ -133,25 +176,25 @@ new Vue({
                         button_bg: '#16B1AA',
                         icon_image: '',
                         list: [
-                            {
-                                name: '4',
-                                title: '小豬賽跑',
-                                image: './img/activity/pigrun.jpg',
-                                icon_img: './img/activity/pig1.png',
-                                desc: '看穿著號碼衣的小豬們，翹著屁股碰碰跳跳的向前賽跑，觀賽前可以先將自己神聖的一票投給喜歡的小豬，比賽結束後，若您投的小豬是冠軍，就可獲得牧場的驚喜小禮物呦！',
-                                data: ['3/11', '3/13'],
-                                time_period: ["9:00-9:30", "10:00-10:30", "11:00-11:30"],
-                                remain: '6',
-                                reserve: '按我預約'
-                            },
-                            {
-                                name: '4',
-                                title: '小豬相見歡',
-                                image: './img/activity/playwithpig.jpg',
-                                icon_img: '',
-                                desc: '想和可愛的小豬們成為朋友嗎？快來和牠們一起無憂無慮地拍照玩耍，感受小豬們歡樂的氣氛吧！',
-                                data: ['3/11', '3/13'],
-                            },
+                            // {
+                            //     name: '4',
+                            //     title: '小豬賽跑',
+                            //     image: './img/activity/pigrun.jpg',
+                            //     icon_img: './img/activity/pig1.png',
+                            //     desc: '看穿著號碼衣的小豬們，翹著屁股碰碰跳跳的向前賽跑，觀賽前可以先將自己神聖的一票投給喜歡的小豬，比賽結束後，若您投的小豬是冠軍，就可獲得牧場的驚喜小禮物呦！',
+                            //     data: ['3/11', '3/13'],
+                            //     time_period: ["9:00-9:30", "10:00-10:30", "11:00-11:30"],
+                            //     remain: '6',
+                            //     reserve: '按我預約'
+                            // },
+                            // {
+                            //     name: '4',
+                            //     title: '小豬相見歡',
+                            //     image: './img/activity/playwithpig.jpg',
+                            //     icon_img: '',
+                            //     desc: '想和可愛的小豬們成為朋友嗎？快來和牠們一起無憂無慮地拍照玩耍，感受小豬們歡樂的氣氛吧！',
+                            //     data: ['3/11', '3/13'],
+                            // },
                         ]
                     },
                     {
@@ -188,33 +231,71 @@ new Vue({
     },
     mounted() {
         //ajax
-        fetch('./php/activity.php')
+        fetch('./php/activity.php', {
+            method: "POST",
+            headers: {
+                "Contene-Type": "application/json",
+            },
+
+            body: JSON.stringify({
+                title: this.all_data[this.activeIndex],
+            }),
+        })
+
+            //title: this.all_data[this.activeIndex],
+
             .then(res => res.json())
             .then((res) => {
-
-                // [
-                //      {
-                //                     name: '5',
-                //                     title: '馬術秀',
-                //                     image: './img/activity/horseshow.jpg',
-                //                     icon_img: './img/activity/horse1.png',
-                //                     desc: '由專業的騎士親自演出，在駿馬馬背上站立、翻騰、跳躍，甚至是高難度的射箭。精彩的馬術秀絕對不容錯過，快來一睹馬兒們的神勇風采吧。',
-                //                     data: ['3/11', '3/13'],
-                //                     time_period: ["13:00-14:00", "15:00-16:00"],
-                //                     remain: '2',
-                //                     reserve: '按我預約',
-                //                     cat:'horse'
-                //                 },
-                // ]
-                console.log(res);
+                this.all_data = res;
+                this.showList = this.all_data;
 
 
 
-                const data = res.data
 
 
-                // data
-                // this.all_data = res
+                // for (let i = 0; i < 6; i++) {
+                //     console.log(res);
+                //     vm.all_data[i].list = res
+
+                //     // var categoryList = []; 存最終結果
+
+                //     //res按照CATEGORY分類
+                //     var categoryContainer = {}; //針對CATEGORY分類的容器
+                //     res.forEach(item => {
+                //         categoryContainer[item.CATEGORY] = categoryContainer[item.CATEGORY] || [];
+                //         categoryContainer[item.CATEGORY].push(item);
+                //     });
+
+                //     console.log(categoryContainer); //按照CATEGORY分類完成
+                //     // if else CA=sheep push sheep ary
+                // }
+                // vm.showList = vm.all_data[0].list;
+                // console.log(Object.keys(categoryContainer)); //物件的key:sheep.deer...
+                //k[j]=keyNme
+
+                // for (j = 0; j < Object.keys(categoryContainer).length; j++) {
+
+                //     vm.all_data[`${Object.keys(categoryContainer)[j]}`].list =
+                //         categoryContainer[`${Object.keys(categoryContainer)[j]}`];
+                // };
+
+
+                // for (let j = 0; j < CATEGORY.length; j++) {
+
+
+                // let ary = [];
+
+                // res.forEach(item => ary.push(vm.all_data[j].title));
+                // console.log(ary);
+                //     vm.all_data[CATEGORY].list = categoryContainer.CATEGORY;
+                // }
+
+
+
+
+
+                // vm.all_data[].list = res
+                // console.log(res);
             })
 
         //分類動物種類 放進去 
@@ -222,6 +303,9 @@ new Vue({
         //.then(res => console.log(res))
     },
     methods: {
+        test(type) {
+            this.showList = this.all_data.filter(e => e.CATEGORY === type);
+        },
         open(data) {
             //console.log(data);
             this.modal_open = data
@@ -230,17 +314,44 @@ new Vue({
             this.modal_open = null
         },
 
+
+
+        // activeIndexSwitch() {
+        //     fetch('./php/activity.php', {
+        //         method: "POST",
+        //         headers: {
+        //             "Contene-Type": "application/json",
+        //         },
+
+        //         body: JSON.stringify({
+
+        //         .then(res => res.json())
+        //         .then((res) => {
+        //             console.log(res);
+        //             for (let i = 0; i < 5; i++) {
+        //                 vm.all_data[i].list = res
+        //             }
+
+
+
+        //         })
+
+
+        // }
+
+
+
         //game
-        gameEntrance() {
-            if ($(window).scrollTop() > 50) {
-                $(".game").addClass('show');
-            } else {
-                $(".game").removeClass('show');
-            }
-        }
+        // gameEntrance() {
+        //     if ($(window).scrollTop() > 50) {
+        //         $(".game").addClass('show');
+        //     } else {
+        //         $(".game").removeClass('show');
+        //     }
+        // }
     }
 });
 
-window.addEventListener("scroll", function () {
-    app.gameEntrance();
-});
+// window.addEventListener("scroll", function () {
+//     app.gameEntrance();
+// });
