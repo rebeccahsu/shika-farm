b7a70c26481e95de69248704e9f9527e8a75a848
charmeee13 <yuin1995@gmail.com>
2022-04-25 03:28:44 +0800

    feat:訂單詳情基本vue

---
+++ src/js/back_order_detail.js
@@ -0,0 +1,45 @@
+//目前頁面亮燈
+let pages = document.querySelector(".aside_ul").querySelectorAll("h5");
+pages.forEach(function (page) {
+    if (page.innerHTML == "訂單管理") {
+        page.closest("a").classList.add("-on");
+    }
+});
+
+new Vue({
+    el: '#app',
+    data: {
+        orderDetils: [
+            {
+                ORDER_ID: '13',
+                UNIT_PRICE: '1331',
+                QUANTITY: '13',
+            },
+        ],
+
+        // order_detail:{},
+        // member:{},
+        // product:{},
+    },
+    // created(){
+    //     const params = new URLSearchParams(location.search);
+    //     let id = params.get('order_id');
+    //     fetch('./php/back_order_detail.php', {
+    //         method: "POST",
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify(
+    //             {id: id}
+    //         ),
+    //     })
+    //     .then(res => res.json())
+    //     .then(res => this.order_detail = res)
+    // },
+
+});
+
+
+
+
+
