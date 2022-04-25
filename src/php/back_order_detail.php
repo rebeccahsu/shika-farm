8fb0b16aa9f6e3e71d2e0fc7c983510e308d95c2
charmeee13 <yuin1995@gmail.com>
2022-04-25 20:19:28 +0800

    feat:訂單詳情ajax串接

--- src/php/back_order_detail.php
+++ src/php/back_order_detail.php
@@ -2,8 +2,17 @@
     include ("./connection.php");
     
     $body = json_decode(file_get_contents("php://input"), true);
+
+    // $sql = 'SELECT o.*, p.ID, p.IMG, p.NAME, m.*, od.QUANTITY, od.UNIT_PRICE
+    // FROM ORDER_DETAIL od JOIN `ORDER` o
+    // ON od.ORDER_ID = o.ID
+    // JOIN PRODUCT p
+    // ON od.PRODUCT_ID = p.ID
+    // JOIN MEMBER m
+    // ON m.ID = o.MEMBER_ID
+    // WHERE o.ID = :id;';
     
-    $sql = 'SELECT o.*, p.ID, p.IMG, od.QUANTITY, od.UNIT_PRICE
+    $sql = 'SELECT o.*, p.ID, p.MAIN_PIC, od.QUANTITY, od.UNIT_PRICE
             FROM ORDER_DETAIL od JOIN `ORDER` o
             ON od.ORDER_ID = o.ID
             JOIN PRODUCT p
