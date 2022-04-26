8570f7d674053171986919798278c997d1373a74
rebeccahsu <rebeccahsuhth@gmail.com>
2022-04-26 23:32:56 +0800

    fix: 最新消息內文給id

--- src/php/news_in.php
+++ src/php/news_in.php
@@ -3,10 +3,10 @@
     
     $body = json_decode(file_get_contents("php://input"), true);
     
-    $sql = "SELECT * FROM NEWS;";
+    $sql = "SELECT * FROM NEWS WHERE ID = :id;";
 
     $stmt = $pdo->prepare($sql);
-    // $stmt->bindValue(":id", $body["id"]);
+    $stmt->bindValue(":id", $body["id"]);
     $stmt->execute();
     $result = $stmt->fetchAll();
     
