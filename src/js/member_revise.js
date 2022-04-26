9f280d41160c8f16bb62c2db1134674644f2867a
rebeccahsu <rebeccahsuhth@gmail.com>
2022-04-26 22:00:21 +0800

    fix: 縮短登出跳轉秒數

--- src/js/member_revise.js
+++ src/js/member_revise.js
@@ -209,7 +209,7 @@ $('#logout').on('click', function () {
       })
         setTimeout(() => {
           location.href = 'index.html';
-        }, 1500)
+        }, 1000)
         
       }
     }
