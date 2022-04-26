a092714fff8bc9d7f35be1a084e8ffea106f12e1
rebeccahsu <rebeccahsuhth@gmail.com>
2022-04-26 11:59:41 +0800

    fix: 後臺最新消息 bug fix

--- src/js/back_news.js
+++ src/js/back_news.js
@@ -25,101 +25,62 @@ function sAlert(msg, icon, btn) {
     })
 }
 
-// 刪除按鈕
-
-// $(function () {
-//     $('#remove_news').on('click', (e) => {
-//         e.preventDefault;
-//         // console.log($(".list_item"));
-//         if (confirm("確定要刪除資料嗎？")) {
-
-//             for (let i = 0; i < $('input[type="checkbox"]').length; i++) {
-//                 // console.log($('input[type="checkbox"]')[i].checked);
-//                 if ($('input[type="checkbox"]')[i].checked) {
-//                     let li_el = $('input[type="checkbox"]')[i].closest(".list_in");
-//                     li_el.remove();
-//                 }
-//             }
-//             // 更新SS
-//             let news_list = $('.list_in');
-//             let task = [];
-//             $(news_list).each((i, v) => {
-//                 // console.log(i);
-//                 // console.log(v);
-//                 var news_artc = {
-//                     news_number: `${i + 1}`,
-//                     news_title: `${$('.list_in')[i].querySelectorAll('p')[0].innerText}`, // <-> "varchar"
-//                     news_img: `${$('.list_in')[i].querySelectorAll('img')[0].getAttribute("src")}`, //(主圖網址) "./img/producds/${品號}_top01"  <-> "varchar"
-//                     news_time: `${$('.list_in')[i].querySelectorAll('p')[1].innerText}`,
-//                     news_text: "糖、香料、美好的事物和小女孩",
-//                 }
-
-//                 task.push(news_artc);
-//             })
-//             sessionStorage.setItem('news_list', JSON.stringify(task));
-
-//         } else {
-//             console.log('取消刪除');
-//         }
-//     })
-// })
-
 
 // 排序
-$('.select_right').on('change', function (e) {
-    let task = JSON.parse(sessionStorage.getItem('news_list'));
-    let ul_el = document.querySelector('.list');
-    let li_el = document.querySelectorAll('.list_in');
-    switch ($('.select_right')[0].value) {
-        case '最新消息':
-            task.sort(function (a, b) {
-                return -(a.news_number - b.news_number);
-            })
-            li_el.forEach((v, i) => { v.remove() });
-            task.forEach((v, i) => {
-                let li_html =
-                    `<li class="list_in" data-news_number="${task[i].news_number}">
-                    <label class="check_container">
-                    <input type="checkbox">
-                    <span class="checkmark"></span>
-                    </label>
-                <img src="${task[i].news_img}" alt="">
-                <p>${task[i].news_title}</p>
-                <p>${task[i].news_time}</p>
-                <button>編輯</button>
-                </li>`
-                // console.log(v);
-                // console.log(i);
-                ul_el.insertAdjacentHTML("beforeend", li_html);
-            })
-            break;
-        case '活動消息':
-            task.sort(function (a, b) {
-                return (a.news_number - b.news_number);
-            })
-            li_el.forEach((v, i) => { v.remove() });
-            task.forEach((v, i) => {
-                let li_html =
-                    `<li class="list_in" data-news_number="${task[i].news_number}">
-                    <label class="check_container">
-                    <input type="checkbox">
-                    <span class="checkmark"></span>
-                    </label>
-                <img src="${task[i].news_img}" alt="">
-                <p>${task[i].news_title}</p>
-                <p>${task[i].news_time}</p>
-                <button>編輯</button>
-                </li>`
-                // console.log(v);
-                // console.log(i);
-                ul_el.insertAdjacentHTML("beforeend", li_html);
-            })
-            break;
-        default:
-            console.log('沒有資料庫');
-    }
+// $('.select_right').on('change', function (e) {
+//     let task = JSON.parse(sessionStorage.getItem('news_list'));
+//     let ul_el = document.querySelector('.list');
+//     let li_el = document.querySelectorAll('.list_in');
+//     switch ($('.select_right')[0].value) {
+//         case '最新消息':
+//             task.sort(function (a, b) {
+//                 return -(a.news_number - b.news_number);
+//             })
+//             li_el.forEach((v, i) => { v.remove() });
+//             task.forEach((v, i) => {
+//                 let li_html =
+//                     `<li class="list_in" data-news_number="${task[i].news_number}">
+//                     <label class="check_container">
+//                     <input type="checkbox">
+//                     <span class="checkmark"></span>
+//                     </label>
+//                 <img src="${task[i].news_img}" alt="">
+//                 <p>${task[i].news_title}</p>
+//                 <p>${task[i].news_time}</p>
+//                 <button>編輯</button>
+//                 </li>`
+//                 // console.log(v);
+//                 // console.log(i);
+//                 ul_el.insertAdjacentHTML("beforeend", li_html);
+//             })
+//             break;
+//         case '活動消息':
+//             task.sort(function (a, b) {
+//                 return (a.news_number - b.news_number);
+//             })
+//             li_el.forEach((v, i) => { v.remove() });
+//             task.forEach((v, i) => {
+//                 let li_html =
+//                     `<li class="list_in" data-news_number="${task[i].news_number}">
+//                     <label class="check_container">
+//                     <input type="checkbox">
+//                     <span class="checkmark"></span>
+//                     </label>
+//                 <img src="${task[i].news_img}" alt="">
+//                 <p>${task[i].news_title}</p>
+//                 <p>${task[i].news_time}</p>
+//                 <button>編輯</button>
+//                 </li>`
+//                 // console.log(v);
+//                 // console.log(i);
+//                 ul_el.insertAdjacentHTML("beforeend", li_html);
+//             })
+//             break;
+//         default:
+//             console.log('沒有資料庫');
+//     }
 
-})
+// })
 
 
 // 搜尋
@@ -183,7 +144,7 @@ new Vue({
                         title: `<h5>您確定要刪除這 ${checked_arr.length} 個最新消息嗎？</h5>`,
                         showCancelButton: true,
                         buttonsStyling: false,
-                        confirmButtonText: '送出',
+                        confirmButtonText: '確定',
                         cancelButtonText: '取消',
                         customClass: {
                             confirmButton: 'btn-green marginright_20',
