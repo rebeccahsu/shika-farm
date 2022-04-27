af67d998c89e68ab64aa12d6807191e4f1620d30
rebeccahsu <rebeccahsuhth@gmail.com>
2022-04-27 23:22:07 +0800

    chore: 上線版本

--- src/php/connection.php
+++ src/php/connection.php
@@ -1,14 +1,14 @@
 <?php
     // MySQL相關資訊
-    $db_host = "127.0.0.1";
-    $db_user = "root";
-    $db_pass = "password";
-    $db_select = "TFD105_G6";
-
     // $db_host = "127.0.0.1";
-    // $db_user = "tibamefe_since2021";
-    // $db_pass = "vwRBSb.j&K#E";
-    // $db_select = "tibamefe_tfd105g6";
+    // $db_user = "root";
+    // $db_pass = "password";
+    // $db_select = "TFD105_G6";
+
+    $db_host = "127.0.0.1";
+    $db_user = "tibamefe_since2021";
+    $db_pass = "vwRBSb.j&K#E";
+    $db_select = "tibamefe_tfd105g6";
 
     //建立資料庫連線物件
     $dsn = "mysql:host=".$db_host.";dbname=".$db_select;
