28c681a6345749edc182ebe9ad8bf3f17f01c3e8
neko0104 <kunpao0104@gmail.com>
2022-04-27 12:51:54 +0800

    docs: 移除多餘的註解

--- src/js/login.js
+++ src/js/login.js
@@ -1,29 +1,3 @@
-// 關閉按鈕和範圍==============
-
-// $('#back_bg').on('click', function (e) {
-//     closebg();
-// })
-
-// $('.bi-x').on('click', function (e) {
-//     closebg();
-// });
-
-// $('#login_box').on('click', function (e) {
-//     e.stopPropagation(e);
-// })
-
-// function closebg(t) {
-//     // console.log(t);
-//     $('#back_bg').remove();
-// }
-
-// 註冊完成、重設密碼完成
-// $('#register_complete').on('click', (e) => {
-//     e.preventDefault();
-    // console.log(e.target);
-    // {前一步送出成功之後，維持登入狀態?}}
-//     closebg();
-// })
 
 // 關閉按鈕和範圍end
 // ============================
@@ -83,7 +57,6 @@ $('#register_password').on('keyup', () => {
         // console.log('c');
         $('label[for="register_password"]').html("<h5>密碼</h5>");
     }
-
 })
 
 
@@ -148,20 +121,6 @@ $('#Street').on('blur', () => {
 
 // 詳細資料驗證end ======================================
 // 驗證end ============================================
-
-/*
-$('#login_btn').on('click', (e) => {
-    e.preventDefault();
-    console.log(e.target);
-})
-
-
-$('#FBlogin_btn').on('click', (e) => {
-    e.preventDefault();
-    console.log(e.target);
-})
-*/
-
 
 // 註冊送出帳密=================================================================
 $('#next').on('click', (e) => {
@@ -260,7 +219,6 @@ $('#send').on('click', (e) => {
                 }
             })
         }else{
-        // alert("還有欄位沒有寫唷！");
         sAlert("還有欄位沒有寫唷！", "warning", "確定");
         // console.log('有資料沒有寫');
     }
@@ -351,44 +309,8 @@ function send_forgetEmail(forgetEmail, forgetName, TOKEN){
 // 忘記密碼時，送出驗證信end ==========================================================
 // ============郵遞區號============
 // const twzipcode = new TWzipcode(".twzipcode");
-// twzipcode.destroy();
+
 let twzip = new TWzipcode('#twzipcode');
-
-// $('#twzipcode').twzipcode({
-//     countyName: 'county', // 設定取得縣市的name
-//     districtName: 'district', // 設定取得鄉鎮市區的name
-//     zipcodeName: 'zipcode', // 設定取得郵遞區號的name
-//     // onDistrictSelect: function () { console.log($('.zipcode').val()); },  //  選擇鄉鎮市區後執行
-// });
-
-/*
-// Initialize
-$(document).ready(function () {
-    const twzipcode = new TWzipcode(".twzipcode");
-    var county = $("select[name='county']").val();  // 取縣市的值
-    var district = $("select[name='district']").val();  // 取鄉鎮市區的值
-    var zipcode = $("select[name='zipcode']").val();  // 取郵遞區號的值
-
-    let get = twzipcode.get();
-
-    $('#twz').TWzipcode({
-        //   css: ['', '', ''], // 套用自訂樣式，順序為縣市 / 鄉鎮市區 / 郵遞區號
-        countyName: 'county', // 設定取得縣市的name
-        districtName: 'district', // 設定取得鄉鎮市區的name
-        zipcodeName: 'zipcode', // 設定取得郵遞區號的name
-        countySel: '台北市', // 縣市預設值
-        districtSel: '信義區', // 鄉鎮市區預設值
-        zipcodeSel: '110', // 郵遞區號預設值
-        hideCounty: [], // 隱藏縣市
-        hideDistrict: [], // 隱藏鄉鎮市區
-        detect: function (coords) { },  //  偵測用戶位置(需支援Google Geo Location API)
-        onCountySelect: function () { },  //  選擇縣市後執行
-        onDistrictSelect: function () { },  //  選擇鄉鎮市區後執行
-        onZipcodeKeyUp: function () { }  //  輸入郵遞區號後執行
-    });
-});
-
-*/
 
 // ==========郵遞區號END============
 // AJAX 登入=========================
@@ -480,11 +402,6 @@ $('.loginBlock').on("click", function (e) {
         $("#register3").addClass("form-off");
     }
 
-
-    // 忘記密碼
-    // if(e.target.classList.contains("send-reset-email")){
-    //     window.location.href = "./password_reset_2.html";
-    // }
 })
 
 
